76c4e9e9c220debc46ff3b36c180da51898fb236
Leonardo Maron <leo22.maron@hotmail.com>
2023-01-20 18:23:01 -0300

    Add new order page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,9 +6,17 @@ class OrdersController < ApplicationController
   end
 
   def new
+    @order = Order.new
   end
 
   def create
+    @order = Order.new(order_params)
+
+    if @order.save
+      redirect_to orders_path
+    else
+      render :new
+    end
   end
 
   # def show
