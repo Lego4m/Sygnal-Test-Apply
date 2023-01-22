72f8414e660920936dc1142a8ae333eeff43ed86
Leonardo Maron <leo22.maron@hotmail.com>
2023-01-22 00:04:12 -0300

    Add disabled options in edit order view

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,12 @@
 module OrdersHelper
+  def get_select_disabled_options(status)
+    case status
+    when 'pending'
+      []
+    when 'in_progress'
+      ['pending']
+    when 'finished'
+      ['pending', 'in_progress']
+    end
+  end
 end
