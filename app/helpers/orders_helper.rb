0996af0fcbb5c8a8bce39fc8aac24472c1882633
Leonardo Maron <leo22.maron@hotmail.com>
2023-01-24 19:01:10 -0300

    Refactor the filters * add .editorConfig

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,12 +1,23 @@
 module OrdersHelper
   def get_select_disabled_options(status)
     case status
-    when 'pending'
-      []
-    when 'in_progress'
-      ['pending']
-    when 'finished'
-      ['pending', 'in_progress']
+      when 'pending'
+        []
+      when 'in_progress'
+        ['pending']
+      when 'finished'
+        ['pending', 'in_progress']
+    end
+  end
+
+  def get_check_box_colors(status)
+    case status
+      when 'pending'
+        "accent-gray-500 text-gray-500 border-gray-500"
+      when 'in_progress'
+        "accent-orange-500 text-orange-500 border-orange-500"
+      when 'finished'
+        "accent-green-500 text-green-500 border-green-500"
     end
   end
 end
