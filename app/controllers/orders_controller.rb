1d5193c327d738b8905581ecfcfb83c0042e03b6
Leonardo Maron <leo22.maron@hotmail.com>
2023-01-25 17:51:47 -0300

    Add validations

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   before_action :set_order, only: [:edit, :update]
+  before_action :verify_status, only: [:update]
 
   def index
     @orders = Order.where(filter_query)
@@ -28,7 +29,7 @@ class OrdersController < ApplicationController
   def update
     if @order.update(order_params)
       redirect_to orders_path
-    else 
+    else
       render :edit
     end
   end
@@ -50,12 +51,16 @@ class OrdersController < ApplicationController
     params.permit(:id, statuses: [])
   end
 
-  def filter_query 
+  def filter_query
     query = {}
 
     query[:id] = params[:id] if params[:id].present?
     query[:status] = params[:statuses] if params[:statuses].present?
 
     return query
+  end
+
+  def verify_status
+    params[:order].extract!(:status) if !Order.can_update_status(@order.status, order_params[:status])
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,26 @@
 class Order < ApplicationRecord
-  enum status: { 
-    pending: 'pending', 
-    in_progress: 'in_progress', 
-    finished: 'finished' 
+  enum status: {
+    pending: 'pending',
+    in_progress: 'in_progress',
+    finished: 'finished'
   }
+
+  validates(:status, {
+    inclusion: {
+      in: ['pending'],
+      message: 'A new order must be in pending status.',
+    },
+    on: :create
+  })
+
+  # Class Methods
+  def self.can_update_status(previousStatus, status)
+    return false if (previousStatus == 'pending' && status != 'in_progress')
+
+    return false if (previousStatus == 'in_progress' && status != 'finished')
+
+    return false if (previousStatus == 'finished')
+
+    return true
+  end
 end
